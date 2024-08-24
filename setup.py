0becf19bc3590d8419f373a6e6a8bf4c65bbee54
pravithota <praveent632@gmail.com>
2024-08-24 18:52:18 +0530

    End to end DL & CI/CD Pipeline with AWS and Azure

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf=8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken_Disease_Classification_Project"
-AUTHOR_USER_NAME = "pravithota"
-SRC_REPO = "cnnClassiifer"
-AUTHOR_EMAIL = "praveent632@gmail.com"
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "entbappy"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
-setuptools.setup (
+
+setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
-    description = "A small python package for CNN APP",
-    long_description= long_description,
-    long_description_content= "text/markdown",
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls = {
+    project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir = {"" : "src"},
-    package = setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
